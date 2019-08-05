b967c14d8ea5c6c88584292ded47a28124f1dd1b
Ashley Cawley <ash@ashleycawley.co.uk>
2019-08-05 13:06:24 +0100

    playing around

--- functions.php
+++ functions.php
@@ -1,5 +1,25 @@
 <?php
 
+// Simple function called 'hey' which echos out
+// a message.
+// Defining the function:
+function hey() {
+    echo "Hello world!";
+}
 
+// Using the function:
+hey();
+
+echo "<br><br>";
+
+echo "sha1 of Hello world! :";
+echo "<br>";
+echo sha1 ('Hello world!');
+
+echo "<br><br>";
+
+echo "String from function:";
+echo "<br>";
+echo sha1 (hey());
 
 ?>
